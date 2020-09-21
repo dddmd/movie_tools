7b9b47e4e62b23314051e9e25a41b5a0498cfe9c
dddmd <dicekkj@gmail.com>
2020-09-22 01:31:08 +0900

    [add func] separate movie

--- script/prot_movie_tool_main.py
+++ script/prot_movie_tool_main.py
@@ -5,10 +5,14 @@ def movie_separate(movie_file, start_sec_list, end_sec_list, offset_sec=0):
 	
 	if not os.path.isfile(movie_file):
 		print(f'{movie_file} file not exist')
+		
+	if len(start_sec_list) != len(end_sec_list):
+		print(f'Erorr sec_lint num, start_sec_list:{len(start_sec_list)}, end_sec_list:{len(end_sec_list)}')
 	
 	file_prefix = re.sub('.h264|.mp4|.MP4|.wmv|.avi','_',os.path.basename(movie_file))
 	print(file_prefix)
 	
+	# Read　Movie　File
 	font = cv2.FONT_HERSHEY_SIMPLEX
 	cap = cv2.VideoCapture(movie_file)
 	width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -16,7 +20,28 @@ def movie_separate(movie_file, start_sec_list, end_sec_list, offset_sec=0):
 	fps = cap.get(cv2.CAP_PROP_FPS)
 	count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 	print('cap:', width, height, fps, count)
+	cap.release()
 	
+	for stime, etime in zip(start_sec_list, end_sec_list):
+		stime_f=stime*fps
+		etime_f=etime*fps
+		cap = cv2.VideoCapture(movie_file)
+		cap.set(cv2.CAP_PROP_POS_FRAMES, stime_f)
+		fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v') #mp4
+		writer = cv2.VideoWriter(f'{file_prefix}_{stime}_{etime}.mp4', fmt, fps, (width,height)) # ライター作成
+
+		frame_pos=stime_f
+		while(frame_pos<etime_f):
+			ret, frame = cap.read()
+			if not ret:
+				break
+				
+			writer.write(frame)
+				
+			frame_pos+=1
+		writer.release()
+	
+	"""
 	frame_pos=0
 	while(cap.isOpened()):
 		
@@ -30,9 +55,10 @@ def movie_separate(movie_file, start_sec_list, end_sec_list, offset_sec=0):
 			break
 		
 		frame_pos+=1
-		
-	cap.release()
-	cv2.destroyAllWindows()
 	
+	cap.release()
+	"""
+	cv2.destroyAllWindows()
+
 if __name__ == '__main__':
-	movie_separate('../data/mov_hts-samp009.mp4', [], [])
+	movie_separate('data/mov_hts-samp009.mp4', [10,15], [12,18])
